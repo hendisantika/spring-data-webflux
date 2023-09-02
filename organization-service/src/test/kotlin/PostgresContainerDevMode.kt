4c0d2ec18b0ffd2d18b3df57aa4e858547eee86f
hendisantika <hendisantika@yahoo.co.id>
2023-09-02 08:07:13 +0700

    #36 - Add PostgresContainerDevMode ✏️📗:octocat:⬆

---
+++ organization-service/src/test/kotlin/PostgresContainerDevMode.kt
@@ -0,0 +1,26 @@
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.context.annotation.Bean
+import org.testcontainers.containers.PostgreSQLContainer
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-data-webflux
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/2/23
+ * Time: 08:06
+ * To change this template use File | Settings | File Templates.
+ */
+@TestConfiguration
+class PostgresContainerDevMode {
+
+    @Bean
+    @ServiceConnection
+    fun postgresql(): PostgreSQLContainer<*>? {
+        return PostgreSQLContainer("postgres:15-alpine")
+                .withUsername("hendi")
+                .withPassword("hendi34")
+    }
+}
