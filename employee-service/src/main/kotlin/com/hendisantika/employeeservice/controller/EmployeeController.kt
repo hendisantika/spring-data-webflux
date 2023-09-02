bed1487e70f6e5a478761b913fb5b9d14e34094b
hendisantika <hendisantika@yahoo.co.id>
2023-09-02 07:43:24 +0700

    #11 - Add findById in EmployeeController in Employee Service ✏️📗:octocat:⬆

--- employee-service/src/main/kotlin/com/hendisantika/employeeservice/controller/EmployeeController.kt
+++ employee-service/src/main/kotlin/com/hendisantika/employeeservice/controller/EmployeeController.kt
@@ -4,9 +4,11 @@ import com.hendisantika.employeeservice.model.Employee
 import com.hendisantika.employeeservice.repostory.EmployeeRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +29,7 @@ class EmployeeController {
 
     @GetMapping
     fun findAll(): Flux<Employee> = employeeRepository.findAll()
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): Mono<Employee> = employeeRepository.findById(id)
 }
