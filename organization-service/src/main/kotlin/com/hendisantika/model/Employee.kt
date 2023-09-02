bdc1f263a9abdc44928a2eba1ccdc33ec7779b41
hendisantika <hendisantika@yahoo.co.id>
2023-09-02 07:59:16 +0700

    #25 - Add Employee Data Class in organization-service ✏️📗:octocat:⬆

---
+++ organization-service/src/main/kotlin/com/hendisantika/model/Employee.kt
@@ -0,0 +1,13 @@
+package com.hendisantika.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-data-webflux
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/2/23
+ * Time: 07:58
+ * To change this template use File | Settings | File Templates.
+ */
+data class Employee(val id: Int, val name: String, val salary: Int, val organizationId: Int)
