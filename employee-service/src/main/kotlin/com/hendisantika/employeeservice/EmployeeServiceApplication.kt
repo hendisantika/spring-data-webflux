191c6d8a179bd502f5d8eb097b466a45750d6117
hendisantika <hendisantika@yahoo.co.id>
2023-09-02 07:39:57 +0700

    #6 - Add initializer in Employee Service ✏️📗:octocat:⬆

--- employee-service/src/main/kotlin/com/hendisantika/employeeservice/EmployeeServiceApplication.kt
+++ employee-service/src/main/kotlin/com/hendisantika/employeeservice/EmployeeServiceApplication.kt
@@ -1,10 +1,23 @@
 package com.hendisantika.employeeservice
 
+import io.r2dbc.spi.ConnectionFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.io.ClassPathResource
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
 @SpringBootApplication
-class EmployeeServiceApplication
+class EmployeeServiceApplication {
+    @Bean
+    fun initializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer? {
+        val initializer = ConnectionFactoryInitializer()
+        initializer.setConnectionFactory(connectionFactory)
+        initializer.setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("schema.sql")))
+        return initializer
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<EmployeeServiceApplication>(*args)
