88afb95a152b8481b43bf53c9bdb5247ea2cc297
hendisantika <hendisantika@yahoo.co.id>
2023-09-02 08:03:09 +0700

    #32 - Add findById in OrganizationController in organization-service ✏️📗:octocat:⬆

--- organization-service/src/main/kotlin/com/hendisantika/controller/OrganizationController.kt
+++ organization-service/src/main/kotlin/com/hendisantika/controller/OrganizationController.kt
@@ -4,10 +4,12 @@ import com.hendisantika.model.Organization
 import com.hendisantika.repository.OrganizationRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,4 +33,7 @@ class OrganizationController {
 
     @GetMapping
     fun findAll(): Flux<Organization> = repository.findAll()
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): Mono<Organization> = repository.findById(id)
 }
